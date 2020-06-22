43709261d79deb40a56f53040d9faa64b47e36d9
Cen Wang <cenwang@umass.edu>
2020-06-22 18:43:58 -0400

    Update README.md

--- transform.py
+++ transform.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 
-def log(x):
+def log_nvm(x):
     return np.log(x).reshape(-1, 1)
+
+
+def log_nvl(x):
+    n, t = x.shape
+    return np.log(x).reshape(n, t, 1)
 
 
 def exp(x):
